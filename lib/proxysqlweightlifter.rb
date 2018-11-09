81ba31cb710ce96a8bdcf7639e7f838ee0dc100f
Ernestas Poskus <ernestas.poskus@gmail.com>
2018-11-09 22:35:53 +0200

    Implement proxysql weight lifter logic

--- lib/proxysqlweightlifter.rb
+++ lib/proxysqlweightlifter.rb
@@ -1,6 +1,21 @@
+require 'mysql2'
 require "proxysqlweightlifter/version"
+require "proxysqlweightlifter/lift"
 
 module Proxysqlweightlifter
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.run(argv)
+    host, port, user, pass, weight = argv
+    Lift.new(
+      Mysql2::Client.new(
+        host: host,
+        port: port,
+        username: user,
+        password: pass,
+        database: 'main'
+      ),
+      weight || 9
+    ).run
+  end
 end
---
+++ lib/proxysqlweightlifter/lift.rb
@@ -0,0 +1,86 @@
+module Proxysqlweightlifter
+  # Lift is responsible for resolving current slaves from
+  # mysql_replication_hostgroups table
+  class Lift
+    attr_reader :db, :weight
+
+    def initialize(database, weight = 10)
+      @db = database
+      @weight = weight
+    end
+
+    def run
+      # We reset all weights to 1 because during runs of
+      # lift master might have switched
+      reset_weight
+
+      hostgroups.each do |hostgroup|
+        find_slaves(hostgroup).map do |slave|
+          slave.delete(:weight)
+          update_weight(slave)
+        end
+
+        debug find_slaves(hostgroup)
+      end
+
+      begin
+        # db.query('LOAD MYSQL SERVERS TO RUNTIME;')
+      rescue => e
+        puts "LOAD MYSQL SERVERS TO RUNTIME error #{e}"
+      end
+    end
+
+    def hostgroups
+      db.query(%(
+        SELECT writer_hostgroup,reader_hostgroup
+        FROM mysql_replication_hostgroups;
+      )).map { |slave| Hash[slave.map{|(k,v)| [k.to_sym,v]}] }
+    end
+
+    def find_master(writer_hostgroup)
+      master = db.query(%(
+        SELECT hostname
+        FROM mysql_servers
+        WHERE hostgroup_id IN (#{writer_hostgroup});
+      ))
+      if master.size > 1
+        raise "More than 1 `writer` #{master} found in #{writer_hostgroup}"
+      end
+      master.map { |m| m['hostname'] }
+    end
+
+    def find_slaves(writer_hostgroup:, reader_hostgroup:)
+      master = find_master(writer_hostgroup)
+      master_ip = master.first
+
+      slaves = db.query(%(
+        SELECT hostgroup_id, hostname, port, weight
+        FROM mysql_servers
+        WHERE hostgroup_id IN (#{writer_hostgroup}, #{reader_hostgroup}) AND hostname != "#{master_ip}";
+      ))
+      if slaves.size.zero?
+        debug "No readers #{slaves} found in hostgroup `writer` #{writer_hostgroup} `reader` #{reader_hostgroup}"
+      end
+      slaves.map { |slave| Hash[slave.map{|(k,v)| [k.to_sym,v]}] }
+    end
+
+    def reset_weight(weight = 1)
+      db.query("UPDATE mysql_servers SET weight = #{weight};")
+    end
+
+    def update_weight(hostname:, port:, hostgroup_id:)
+      db.query(%(
+        UPDATE mysql_servers
+        SET weight = #{weight || 1}
+        WHERE hostname = "#{hostname}" AND port = "#{port}" AND hostgroup_id = "#{hostgroup_id}";
+      ))
+    end
+
+    private
+
+    def debug(s)
+      return
+      puts s
+    end
+  end
+end
